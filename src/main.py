b0c280cf9e0a09ed832dff1503f87c4b05066f75
ThisismyEODev <nastasja.development@gmail.com>
2021-05-27 14:16:03 +0200

    added empty init file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 27 14:02:00 2021
+Main file of the deep learning based landcover classification challenge.
+
+
+For more information, see project's GitLab repo:
+
+    https://github.com/ThisismyEODev/DeepLearningLandcoverClassification.git
+
 
 @author: ThisismyEODev
+@email: nastasja.development@gmail.com
 """
+
+def main() -> None:
+    """Run module from command line."""
+
+
+
+
+if __name__ == "__main__":
+    main()
+
