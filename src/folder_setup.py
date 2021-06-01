1f1a18511601a11f0d8b54b3956663ea97c08538
ThisismyEODev <nastasja.development@gmail.com>
2021-06-01 09:51:51 +0200

    changed the folder setup file a bt

--- src/folder_setup.py
+++ src/folder_setup.py
@@ -7,7 +7,7 @@
 """
 
 from pathlib import Path
-
+import parameter_file as parameters
 
 def setup_workspace(path):
     """
@@ -18,18 +18,27 @@ def setup_workspace(path):
     path : str or Path
         path to where we want to work.
 
-    Returns
-    -------
-    None.
-
     """
     if isinstance(path, str):
         path = Path(path)
+    elif isinstance(path, Path):
+        path = path
+    print("The Setup will occur here: ", str(path))
 
     data_path = path / 'inputdata'
-    # if data_path is empty... download
+    if data_path.is_dir() is False:
+        print("Create the data directory")
+        data_path.mkdir()
 
-    model_path = path / 'model'
-    results_path = path / 'results'
+    model_path = path / 'model_directory'
+    if model_path.is_dir() is False:
+        print("Create the directory for storing models")
+        model_path.mkdir()
 
 
+def main() -> None:
+    setup_workspace(parameters.path)
+
+
+if __name__ == "__main__":
+    main()
